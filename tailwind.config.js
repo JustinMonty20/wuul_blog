8c117941fd29357c05dc7587b88c4bf37ecd12df
JustinMonty20 <montgomerycodepro@gmail.com>
2024-01-07 14:21:03 -0500

    more colors and sizes for my dev blob added onto tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    plugins: [],
     extend: {
+      textColor: {
+        "wuul-main": "#333333",
+        "wuul-bg": "#F9F6EE",
+        "wuul-accent": "#552583",
+        "wuul-gold": "#FDB927"
+      },
       colors: {
         "wuul-green": "#63996A",
-        "wuul-txt": "#333333"
+      },
+      width: {
+        "wuul-1/2": "50%"
+      },
+      fontFamily: {
+        "main": ["Montserrat", "sans-serif"],
+        "sub": ["Merriweather", "sans"]
       }
     },
   },
