588eec0683499460fdf60b867754cf7922b42db2
JustinMonty20 <montgomerycodepro@gmail.com>
2024-01-06 21:00:31 -0500

    tailwind design system for my custom 'wuul' util classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "wuul-green": "#63996A",
+        "wuul-txt": "#333333"
+      }
+    },
   },
   plugins: [],
 }
