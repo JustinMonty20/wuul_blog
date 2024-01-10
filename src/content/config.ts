dcf9ed1fc863325db20f03240f507058f75771d1
JustinMonty20 <montgomerycodepro@gmail.com>
2024-01-09 22:56:52 -0500

    content config && updates to all the previous compoennts. Use semantic html & responsiveness

---
+++ src/content/config.ts
@@ -0,0 +1,4 @@
+import { z, defineCollection } from 'astro:content';
+
+// define my type declarations for my content here.
+// I think I'll break up my content into the different sections that I'm going to be writing about.
