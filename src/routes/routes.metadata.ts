c35441b310a649682d41468224094600a304adcb
JustinMonty20 <montgomerycodepro@gmail.com>
2024-01-27 22:34:31 -0500

    one set of routes and just altering the depth on certain pages / layouts

--- src/routes/routes.metadata.ts
+++ src/routes/routes.metadata.ts
@@ -1,4 +1,13 @@
-import type { NavMetadata } from "../types";
-// TODO: improve this.
+import type { NavMetadata, BlogRoutes } from "../types";
+
 export const routes: NavMetadata[] = [{to: "/", txt: "About"}, {to: "/blogs", txt: "Blogs"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}];
-export const deepNestedRoutes:NavMetadata[] = [{to: "../../", txt: "About" }, {to: "../../blogs", "txt": "Blogs"}, {to: "../../../showcase", txt: "Showcase"}, {to: "../../vouches", txt: "Vouches"}]
+
+export const blogRoutes: BlogRoutes = {
+    base: [{to: "/", txt: "About"}, {to: "/blogs", txt: "Blogs"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}],
+    alterBaseRouteDepth: function (level: string) {
+        return this.base.map((r) => ({
+            to: `${level}` + r.to,
+            txt: r.txt
+        }))
+    } 
+}
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,8 @@ export interface NavMetadata {
     to:string,
     txt:string
 }
+
+export interface BlogRoutes {
+    base: NavMetadata[],
+    alterBaseRouteDepth: (level: string) => NavMetadata[]
+}
