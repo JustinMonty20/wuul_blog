3487147e55092c17703cc3dba7a48eeeef4a1469
JustinMonty20 <montgomerycodepro@gmail.com>
2024-02-17 21:12:33 -0500

    updating the text for my routes

--- src/routes/routes.metadata.ts
+++ src/routes/routes.metadata.ts
@@ -1,9 +1,9 @@
 import type { NavMetadata, BlogRoutes } from "../types";
 
-export const routes: NavMetadata[] = [{to: "/", txt: "About"}, {to: "/blogs", txt: "Blogs"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}];
+export const routes: NavMetadata[] = [{to: "/", txt: "About"}, {to: "/blog", txt: "Blog"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}];
 
 export const blogRoutes: BlogRoutes = {
-    base: [{to: "/", txt: "About"}, {to: "/blogs", txt: "Blogs"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}],
+    base: [{to: "/", txt: "About"}, {to: "/blog", txt: "Blog"}, {to: "/showcase", txt: "Showcase"}, {to: "/vouches", txt: "Vouches"}],
     alterBaseRouteDepth: function (level: string) {
         return this.base.map((r) => ({
             to: `${level}` + r.to,
